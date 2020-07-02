83c042892dc7e7b911bcbbf78a0f847242a82e61
MohammadFraz <frazmohammed@gmail.com>
2020-07-02 14:08:42 +0530

    initial commit

---
+++ GeneralSuggestions/src/main/java/com/generalsuggestions/GeneralSuggestionsApplication.java
@@ -0,0 +1,13 @@
+package com.generalsuggestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralSuggestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralSuggestionsApplication.class, args);
+	}
+
+}
---
+++ GeneralSuggestions/src/test/java/com/generalsuggestions/GeneralSuggestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generalsuggestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralSuggestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
