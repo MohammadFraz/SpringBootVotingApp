7b0e383e4119227931045096f72239051558d3d0
MohammadFraz <frazmohammed@gmail.com>
2020-07-13 17:30:20 +0530

    creating the custom login page

--- GeneralSuggestions/src/main/java/com/generalsuggestions/web/DashboardController.java
+++ GeneralSuggestions/src/main/java/com/generalsuggestions/web/DashboardController.java
@@ -1,4 +1,4 @@
-package com.generalsuggestions;
+package com.generalsuggestions.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
